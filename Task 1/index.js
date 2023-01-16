5a293eb62d090d3e52d00323eb6fa8b4345e1eef
Abanoub <ayakoub@kaiilab.com>
2023-01-17 01:28:56 +0200

    first task

---
+++ Task 1/index.js
@@ -0,0 +1,33 @@
+
+// one
+////////
+function sum(sum1=0, sum2=0) {
+  let result = sum1 + sum2;
+  console.log(result);
+}
+sum(3, 2);
+sum(-3, -6);
+sum(7, 3);
+//
+// Two
+////////                              
+function triangle(base, height) {
+    let result = (base * height) / 2;
+    return result;
+
+}
+let msg = triangle(3, 2);
+let msg1 = triangle(7, 4);
+let msg2 = triangle(10, 10);
+console.log(msg);
+console.log(msg1);
+console.log(msg2);
+//
+// Three
+//////// 
+function something(some ) {
+  console.log(" something " + some);
+}
+something("is better than nothing");
+something("Bob Jane");
+something("something");
